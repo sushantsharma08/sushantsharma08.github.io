7eb405c6179d20ff5abbc1942f2ac80a0471f57c
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2022-03-26 21:42:48 +0530

    cahnges
    1. contact page with firebase
    2. refactored code

--- JavaScript/script.js
+++
@@ -1,5 +0,0 @@
-const header = document.querySelector('header1')
-const home = document.querySelector('.home');
-const about = document.querySelector('.about');
-const educationalQualification = document.querySelector('.educ');
-const skills_Hobbies = document.querySelector('.skill_hobbies');
---
+++ javaScript/main.js
@@ -0,0 +1,42 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNGo4acKuRRiKnb4ii_8dM1FkJ3O_ZTEE",
+    authDomain: "contact-form-295b4.firebaseapp.com",
+    databaseURL: "https://contact-form-295b4-default-rtdb.firebaseio.com",
+    projectId: "contact-form-295b4",
+    storageBucket: "contact-form-295b4.appspot.com",
+    messagingSenderId: "675479278265",
+    appId: "1:675479278265:web:9499c52234b863570d4168"
+  };
+  //initialize
+  firebase.initializeApp(firebaseConfig);
+
+// referencing messages collection
+var messagesref = firebase.database().ref('infos');
+  // submit form
+const submit = document.getElementById('contactForm').addEventListener('submit', submitForm);
+function submitForm(e){
+    e.preventDefault();
+    
+    let name = getElement('name');
+    let email = getElement('email');
+    let message = getElement('message');
+
+    saveMessage(name, email, message);
+
+    alert('message saved...!');
+}
+
+// getting value function
+function getElement(id){
+  return  document.getElementById(id).value;
+}
+
+//save message to firebase database
+function saveMessage(name, email, message){
+    let newMessageRef = messagesref.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        message: message
+    });
+}
